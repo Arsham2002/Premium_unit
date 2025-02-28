67e816db538da44aacc16b914032aa70140e289e
navidio.h <navidppanahi@gmail.com>
2025-02-28 16:24:25 +0330

    Add ShowInvoiceDetailsController

---
+++ PremiumUnit/Controllers/ShowInvoiceDetailsController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PremiumUnit.Data;
+using PremiumUnit.Models;
+
+namespace PremiumUnit.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private readonly PremiumUnitContext _context;
+
+        public InvoicesController(PremiumUnitContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoice
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Amount,IssueDate,PaymentDate,PenaltyBaseDate,WorkshopCode")] Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(invoice);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvoiceExists(invoice.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index), new { id = WorkshopCode });
+            }
+            return View(invoice);
+        }
+
+        private bool InvoiceExists(int id)
+        {
+            return _context.Invoice.Any(e => e.Id == id);
+        }
+    }
+}
